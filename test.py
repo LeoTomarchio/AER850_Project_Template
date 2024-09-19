e55734a692a25744229cd62f7e5d26687e569549
LeoTomarchio <leandro.tomarchio@torontomu.ca>
2024-09-19 12:48:08 -0400

    Created python file

---
+++ test.py
@@ -0,0 +1,20 @@
+for i in range(1, 11):
+    print(i)
+
+# Example of changing the size of an array
+my_array = [1, 2, 3, 4, 5]
+print("Original array:", my_array)
+
+# Increase the size of the array by appending elements
+my_array.append(6)
+my_array.extend([7, 8])
+print("Increased size:", my_array)
+
+# Decrease the size of the array by removing elements
+my_array = my_array[:3]
+print("Decreased size:", my_array)
+
+
+
+
+    
