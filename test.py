1bffe9a3a940c1af00816bb136d72ec2530fe91e
LeoTomarchio <leandro.tomarchio@torontomu.ca>
2024-09-20 11:22:33 -0400

    Friday Lecture

--- test.py
+++ test.py
@@ -1,20 +1,27 @@
-for i in range(1, 11):
-    print(i)
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
-# Example of changing the size of an array
-my_array = [1, 2, 3, 4, 5]
-print("Original array:", my_array)
+"""OneHotEncoder"""
+df = pd.read_csv('aer850_f24-main/data/housing.csv')
+my_encoder = OneHotEncoder()
+my_encoder.fit(df[['ocean_proximity']])
+encoded_data = my_encoder.transform(df[['ocean_proximity']])
+category_names = my_encoder.get_feature_names_out()
+print(encoded_data)
+encoded_data_df = pd.DataFrame(encoded_data, columns=category_names)
+df = pd.concat([df,encoded_data_df],axis=1)
+df = df.drop(columns=['ocean_proximity'])
+df.to_csv("test2.csv")
 
-# Increase the size of the array by appending elements
-my_array.append(6)
-my_array.extend([7, 8])
-print("Increased size:", my_array)
+"""Define X and Y"""
+x_columns = ["longitude", "latitude", "housing_median_age", "total_rooms", "total_bedrooms", "population", "households", "median_income"]
+y_column = ["median_house_value"]
+X = df[x_columns]
+y = df[y_column]
 
-# Decrease the size of the array by removing elements
-my_array = my_array[:3]
-print("Decreased size:", my_array)
-
-
+"""Train Test Split"""
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
 
-    
